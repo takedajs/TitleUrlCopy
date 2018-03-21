7704083532db33bac443de1fd9e05e93dd71f3b4
takedajs <you@example.com>
2018-03-22 07:49:44 +0900

    factor: option add

---
+++ options.js
@@ -0,0 +1,24 @@
+//ラジオボタンを変更した時に実行
+document.getElementById("copy").onchange = function(){
+
+    var copy = document.getElementsByName("copy");
+
+    var checkedCopy = "";
+    for (var i = 0; i < copy.length; i++) {
+        //チェックした値を取得
+        if (copy[i].checked) {
+            checkedCopy = copy[i].value;
+            break;
+        }
+    }
+
+    //チェックした要素をストレージ格納
+    browser.storage.local.set({
+        'checkCopy': checkedCopy
+    });
+}
+
+//ストレージに格納されている情報をラジオボタンに反映
+browser.storage.local.get('checkCopy', function(res) {
+    document.getElementById(res.checkCopy).checked = true;
+});
