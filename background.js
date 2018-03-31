a5c62239cd16571c117ce2330733cab214cc55b2
takedajs <you@example.com>
2018-03-31 17:54:40 +0900

    factor: 全タブ情報を取得

---
+++ background.js
@@ -0,0 +1,24 @@
+var titleUrls = [];
+
+// 全タブのタイトルとURLをストレージに保存する
+function setTitleUrls(tabs) {
+    for (var i = 0; i < tabs.length; i++){
+        titleUrls[i] = {
+            "title": tabs[i].title,
+            "url": tabs[i].url,
+        }
+    }
+
+    //ストレージに保存
+    browser.storage.local.set({
+        'titleUrls': titleUrls
+    });
+
+}
+
+window.setInterval(
+    function(){
+        browser.tabs.query({}).then(setTitleUrls);
+    },
+    1000
+);
